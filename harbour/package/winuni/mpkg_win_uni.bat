24ed33e7e93c266a125edf3ea51189ddac1ab982
Viktor Szakats <vszakats@users.noreply.github.com>
2015-03-25 10:54:11 +0100

    v3.0.0 final

--- harbour/package/winuni/mpkg_win_uni.bat
+++ harbour/package/winuni/mpkg_win_uni.bat
@@ -16,10 +16,10 @@ rem - Run this from vanilla official source tree only.
 
 echo ! Self: %0
 
-if "%HB_VS%" == "" set HB_VS=21
-if "%HB_VL%" == "" set HB_VL=210
-if "%HB_VM%" == "" set HB_VM=2.1
-if "%HB_VF%" == "" set HB_VF=2.1.0rc2
+if "%HB_VS%" == "" set HB_VS=30
+if "%HB_VL%" == "" set HB_VL=300
+if "%HB_VM%" == "" set HB_VM=3.0
+if "%HB_VF%" == "" set HB_VF=3.0.0
 if "%HB_RT%" == "" set HB_RT=C:\hb\
 
 set HB_DR=hb%HB_VS%\
