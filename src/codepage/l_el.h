c95afde46ab61b77490801b61d208e8407531093
petewg <pete_west@yahoo.gr>
2016-01-13 11:50:11 +0200

    code pge correction/completion

--- src/codepage/l_el.h
+++ src/codepage/l_el.h
@@ -1,4 +1,5 @@
 /* Pete Dionisopoulos <pete_westg@yahoo.gr> */
 
-#define HB_CP_UPPER     "Α~ΆΒΓΔΕ~ΈΖΗ~ΉΘΙ~ΊΚΛΜΝΞΟ~ΌΠΡΣΣΤΥ~ΎΦΧΨΩ~Ώ"
-#define HB_CP_LOWER     "α~άβγδε~έζη~ήθι~ίκλμνξο~όπρσςτυ~ύφχψω~ώ"
+#define HB_CP_UPPER     "Α~ΆΒΓΔΕ~ΈΖΗ~ΉΘΙ~Ί~ΪΚΛΜΝΞΟ~ΌΠΡΣΣΤΥ~Ύ~ΫΦΧΨΩ~Ώ"
+#define HB_CP_LOWER     "α~άβγδε~έζη~ήθι~ί~ϊκλμνξο~όπρσςτυ~ύ~ϋφχψω~ώ"
+
