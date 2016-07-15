harbour/core

commit 8a2fd4c0656db6ba898451741e38850e84b8a0a3

2016-01-24 22:47 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbcairo/hbcairo.hbp   * contrib/hbfimage/hbfimage.hbp   * contrib/hbgd/hbgd.hbp   * contrib/hbgs/hbgs.hbp   * contrib/hbhpdf/3rd/libhpdf/libhpdf.hbp   * contrib/hbmagic/hbmagic.hbp   * contrib/hbmysql/hbmysql.hbp   * contrib/hbpgsql/hbpgsql.hbp   * contrib/hbwin/hbwin.hbp   * contrib/sddmy/sddmy.hbp   * contrib/sddpg/sddpg.hbp     * sync dependency detection paths with `libpaths=` for darwin.       This means switching to the standard location of Homebrew       from the old MacPorts paths. Under Homebrew, `pkg-config`       is used for detection, if installed, though.

contrib/hbpgsql/hbpgsql.hbp
@@ -18,9 +18,8 @@
 -depincpath=pgsql:/usr/local/pgsql/include/libpq
 -depincpath=pgsql:/usr/include/pgsql
 -depincpath=pgsql:/usr/include/postgresql
+-depincpath=pqsql:/usr/local/opt/postgresql/include
 -depincpath=pgsql:/postgres/include
--depincpath=pgsql:/opt/local/include/postgresql84
--depincpath=pgsql:/opt/local/include/postgresql83
 -depimplibs=pgsql:../lib/libpq.dll
 
 -iflag={bcc}-a

contrib/hbcairo/hbcairo.hbp
@@ -18,7 +18,7 @@
 -depcontrol=cairo:${HB_WITH_CAIRO}
 -depincpath=cairo:/usr/include/cairo
 -depincpath=cairo:/usr/local/include/cairo
--depincpath=cairo:/opt/local/include/cairo
+-depincpath=cairo:/usr/local/opt/cairo/include/cairo
 -depimplibs=cairo:../../bin/libcairo-2.dll
 -depimplibd=cairo:cairo
 

contrib/hbfimage/hbfimage.hbp
@@ -18,7 +18,7 @@ hbwin.hbc{allwin}
 -depcontrol=freeimage:${HB_WITH_FREEIMAGE}
 -depincpath=freeimage:/usr/include
 -depincpath=freeimage:/usr/local/include
--depincpath=freeimage:/opt/local/include
+-depincpath=freeimage:/usr/local/opt/freeimage/include
 -depimplibs=freeimage:FreeImage.dll
 
 # For FreeImage headers

contrib/hbgd/hbgd.hbp
@@ -17,7 +17,7 @@ hbct.hbc
 -depcontrol=gd:${HB_WITH_GD}
 -depincpath=gd:/usr/include
 -depincpath=gd:/usr/local/include
--depincpath=gd:/opt/local/include
+-depincpath=gd:/usr/local/opt/gd/include
 -depimplibs=gd:../bin/bgd.dll{!(msvc|pocc|mingw)|hbdyn}
 -depimplibs=gd:../lib/bgd.lib{(msvc|pocc|mingw)&!hbdyn}
 

contrib/hbgs/hbgs.hbp
@@ -18,7 +18,7 @@
 -depincpath=gs:/usr/include
 -depincpath=gs:/usr/include/ghostscript
 -depincpath=gs:/usr/local/include/ghostscript
--depincpath=gs:/opt/local/include/ghostscript
+-depincpath=gs:/usr/local/opt/ghostscript/include
 -depimplibs=gs:${HB_WITH_GS_BIN}/gsdll32.dll{win&x86}
 -depimplibs=gs:${HB_WITH_GS_BIN}/gsdll64.dll{win&x86_64}
 -depimplibs=gs:${HB_WITH_GS_BIN}/gsdll2.dll{os2}

contrib/hbhpdf/3rd/libhpdf/libhpdf.hbp
@@ -16,7 +16,7 @@
 -depcontrol=png:local{HB_BUILD_3RDEXT='no'}
 -depcontrol=png:${HB_WITH_PNG}
 -depincpath=png:/usr/include
--depincpath=png:/opt/local/include
+-depincpath=png:/usr/local/include
 -depincpathlocal=png:../../../../src/3rd/png
 
 -depkeyhead=zlib:zlib.h

contrib/hbmagic/hbmagic.hbp
@@ -13,7 +13,7 @@
 -depcontrol=libmagic:${HB_WITH_LIBMAGIC}
 -depincpath=libmagic:/usr/include
 -depincpath=libmagic:/usr/local/include
--depincpath=libmagic:/opt/local/include
+-depincpath=libmagic:/usr/local/opt/libmagic/include
 
 -hbx=${hb_name}
 

contrib/hbmysql/hbmysql.hbp
@@ -20,7 +20,8 @@
 -depcontrol=mysql:${HB_WITH_MYSQL}
 -depincpath=mysql:/usr/include/mysql
 -depincpath=mysql:/usr/local/include/mysql
--depincpath=mysql:/opt/local/include/mysql5/mysql
+-depincpath=mysql:/usr/local/opt/mariadb/include
+-depincpath=mysql:/usr/local/opt/mysql/include
 -depimplibs=mysql:../lib/opt/libmySQL.dll
 -depimplibs=mysql:../lib/libmySQL.dll
 -depimplibs=mysql:../../lib/libmysql.dll

vszakats/hb

commit ecdce0e9d8ecd9150ecd55c31f7105eda03a33e0

2016-07-15 04:10 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbpgsql/hbpgsql.hbp     ! Fix typo in dependency configuration. Regression from 8a2fd4c0656db6ba898451741e38850e84b8a0a3
  * package/mpkg_win_ci.sh
    * Cleanup MSYS2 dependency setup

contrib/hbpgsql/hbpgsql.hbp
@@ -19,7 +19,7 @@
 -depincpath=pgsql:/usr/local/pgsql/include/libpq
 -depincpath=pgsql:/usr/include/pgsql
 -depincpath=pgsql:/usr/include/postgresql
--depincpath=pqsql:/usr/local/opt/postgresql/include
+-depincpath=pgsql:/usr/local/opt/postgresql/include
 -depincpath=pgsql:/postgres/include
 -depimplibs=pgsql:../lib/libpq.dll
 

package/mpkg_win_ci.sh
@@ -77,6 +77,9 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
    [ "${_BRANCH#*lto*}" != "${_BRANCH}" ] && export HB_USER_CFLAGS="${HB_USER_CFLAGS} -flto -ffat-lto-objects"
    [ "${HB_BUILD_MODE}" = 'cpp' ] && export HB_USER_LDFLAGS="${HB_USER_LDFLAGS} -static-libstdc++"
 
+   readonly _msys_mingw32='/mingw32'
+   readonly _msys_mingw64='/mingw64'
+
    export HB_DIR_MINGW="${HB_RT}/mingw64"
    if [ -d "${HB_DIR_MINGW}/bin" ] ; then
       # Use the same toolchain for both targets
@@ -85,8 +88,8 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
       _build_info_32='BUILD-mingw.txt'
       _build_info_64=/dev/null
    else
-      export HB_DIR_MINGW_32='/mingw32'
-      export HB_DIR_MINGW_64='/mingw64'
+      export HB_DIR_MINGW_32="${_msys_mingw32}"
+      export HB_DIR_MINGW_64="${_msys_mingw64}"
       [ "${HB_BASE}" != '64' ] && HB_DIR_MINGW="${HB_DIR_MINGW_32}"
       [ "${HB_BASE}"  = '64' ] && HB_DIR_MINGW="${HB_DIR_MINGW_64}"
       _build_info_32='BUILD-mingw32.txt'
@@ -106,10 +109,11 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
 
    export HB_WITH_CURL="${HB_DIR_CURL_32}include"
    export HB_WITH_OPENSSL="${HB_DIR_OPENSSL_32}include"
-   export HB_WITH_FREEIMAGE='/mingw32/include'
-   export HB_WITH_GD='/mingw32/include'
-   export HB_WITH_ICU='/mingw32/include'
-   export HB_WITH_PGSQL='/mingw32/include'
+   _inc="${_msys_mingw32}/include"
+   export HB_WITH_FREEIMAGE="${_inc}"
+   export HB_WITH_GD="${_inc}"
+   export HB_WITH_ICU="${_inc}"
+   export HB_WITH_PGSQL="${_inc}"
    export PATH="${HB_DIR_MINGW_32}/bin:${_ori_path}"
    gcc -v 2> "${_build_info_32}"
    # shellcheck disable=SC2086
@@ -117,10 +121,11 @@ if [ "${_BRANC4}" != 'msvc' ] ; then
 
    export HB_WITH_CURL="${HB_DIR_CURL_64}include"
    export HB_WITH_OPENSSL="${HB_DIR_OPENSSL_64}include"
-   export HB_WITH_FREEIMAGE='/mingw64/include'
-   export HB_WITH_GD='/mingw64/include'
-   export HB_WITH_ICU='/mingw64/include'
-   export HB_WITH_PGSQL='/mingw64/include'
+   _inc="${_msys_mingw64}/include"
+   export HB_WITH_FREEIMAGE="${_inc}"
+   export HB_WITH_GD="${_inc}"
+   export HB_WITH_ICU="${_inc}"
+   export HB_WITH_PGSQL="${_inc}"
    export PATH="${HB_DIR_MINGW_64}/bin:${_ori_path}"
    gcc -v 2> "${_build_info_64}"
    # shellcheck disable=SC2086