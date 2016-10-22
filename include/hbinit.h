harbour/core

commit 8045f3a4b1605b9bd2a3ed2552c845b8ddd72d5e

2014-06-26 17:12 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * doc/en/string.txt     * cleanups
  * src/rtl/gtchrmap.c
    % variable scoping

  ; [ Testing @vszakats ]

src/rtl/gtchrmap.c
@@ -53,7 +53,7 @@
 #include "hbgtcore.h"
 #include "hbapifs.h"
 
-#if defined( HB_OS_UNIX ) || defined( HB_OS_DOS )
+#if defined( HB_OS_UNIX ) || defined( HB_OS_DOS ) || 1
 
 #define MAX_CHAR_VAL  0xff
 #define HB_CHRMAP( a, c )  ( ( ( a ) << 16 ) | ( c ) )
@@ -165,7 +165,6 @@ static void skip_blank( char ** buf )
 static int get_val( char ** buf )
 {
    int n = -1;
-   char c;
 
    if( ( *buf )[ 0 ] == '\'' && ( *buf )[ 1 ] != '\0' && ( *buf )[ 2 ] == '\'' )
    {
@@ -178,16 +177,16 @@ static int get_val( char ** buf )
       *buf += 2;
       for(; ( **buf >= '0' && **buf <= '9' ) ||
             ( **buf >= 'A' && **buf <= 'F' ) ||
-            ( **buf >= 'a' && **buf <= 'f' ); ( *buf )++ )
+            ( **buf >= 'a' && **buf <= 'f' ); ++( *buf ) )
       {
-         c = **buf | 0x20;
+         char c = **buf | 0x20;
          n = ( n << 4 ) + c - ( c > '9' ? ( 'a' - 10 ) : '0' );
       }
    }
    else if( **buf >= '0' && **buf <= '9' )
    {
       n = 0;
-      for(; ( **buf >= '0' && **buf <= '9' ); ( *buf )++ )
+      for(; ( **buf >= '0' && **buf <= '9' ); ++( *buf ) )
          n = n * 10 + ( **buf - '0' );
    }
    return n > 0xff ? -1 : n;
@@ -278,7 +277,7 @@ static int parse_line( char * buf, int * from, int * to, char * op, int * val, i
 static int chrmap_parse( FILE * fp, const char * pszTerm, int * nTransTbl, const char * pszFile )
 {
    int line = 0, from = 0, to = 0, val = 0, mod = 0, i, n;
-   char buf[ 256 ], * s, op = 0;
+   char * s, op = 0;
    int isTerm = 0;
    fpos_t pos;
 
@@ -287,6 +286,7 @@ static int chrmap_parse( FILE * fp, const char * pszTerm, int * nTransTbl, const
 
    while( ! feof( fp ) && isTerm < 2 )
    {
+      char buf[ 256 ];
       ++line;
       if( fgets( buf, sizeof( buf ), fp ) != NULL )
       {
@@ -370,14 +370,14 @@ static int chrmap_parse( FILE * fp, const char * pszTerm, int * nTransTbl, const
 
 static int hb_gt_chrmapread( const char * pszFile, const char * pszTerm, int * nTransTbl )
 {
-   FILE * fp;
-   char buf[ 256 ], * ptr, * pTerm;
    int isTerm = -1;
 
-   fp = hb_fopen( pszFile, "r" );
+   FILE * fp = hb_fopen( pszFile, "r" );
 
    if( fp != NULL )
    {
+      char buf[ 256 ], * ptr, * pTerm;
+
       hb_strncpy( buf, pszTerm, sizeof( buf ) - 1 );
       isTerm = 0;
       pTerm = buf;
@@ -399,7 +399,7 @@ static int hb_gt_chrmapread( const char * pszFile, const char * pszTerm, int * n
 
 int hb_gt_chrmapinit( int * piTransTbl, const char * pszTerm, HB_BOOL fSetACSC )
 {
-   char * pszFree = NULL, * pszFile, szFile[ HB_PATH_MAX ];
+   char * pszFree = NULL;
    int nRet = -1;
 
    chrmap_init( piTransTbl );
@@ -415,7 +415,9 @@ int hb_gt_chrmapinit( int * piTransTbl, const char * pszTerm, HB_BOOL fSetACSC )
 
    if( pszTerm != NULL && *pszTerm != '\0' )
    {
-      pszFile = hb_getenv( "HB_CHARMAP" );
+      char szFile[ HB_PATH_MAX ];
+      char * pszFile = hb_getenv( "HB_CHARMAP" );
+
       if( pszFile != NULL && *pszFile != '\0' )
          nRet = hb_gt_chrmapread( pszFile, pszTerm, piTransTbl );
       if( nRet == -1 )
@@ -463,7 +465,7 @@ int main( int argc, char ** argv )
       exit( 1 );
    }
 
-   for( i = 0; i < 256; i++ )
+   for( i = 0; i < 256; ++i )
       printf( "%3d -> %3d : %d\n", i, piTransTbl[ i ] & 0xff, piTransTbl[ i ] >> 16 );
 
    return 0;

vszakats/hb

commit b0932fd5c32597cb60f8553e300ca66ecacecedf

2016-10-22 10:33 UTC+0200 Viktor Szakats (vszakats users.noreply.github.com)   * include/hbinit.h     ! fix missing line after merge.       Regression from bde5c0c6a942276b44221b22ebf94b6d1b3ec182
  * src/rtl/gtchrmap.c
    ! do not compile on all platforms.
      Regression from 8045f3a4b1605b9bd2a3ed2552c845b8ddd72d5e

include/hbinit.h
@@ -60,6 +60,7 @@ extern HB_EXPORT PHB_SYMB hb_vmProcessSymbols( PHB_SYMB pSymbols, HB_USHORT uiSy
    defined but not used static variable initialized with this method. */
 #if defined( __cplusplus ) && ! defined( HB_STATIC_STARTUP ) && \
     ! defined( HB_PRAGMA_STARTUP ) && ! defined( HB_GNUC_STARTUP ) && \
+    ! defined( HB_INITSEG_STARTUP ) && ! defined( HB_DATASEG_STARTUP ) && \
     ! defined( __GNUC__ )
    #define HB_STATIC_STARTUP
 #endif

src/rtl/gtchrmap.c
@@ -53,7 +53,7 @@
 #include "hbgtcore.h"
 #include "hbapifs.h"
 
-#if defined( HB_OS_UNIX ) || defined( HB_OS_DOS ) || 1
+#if defined( HB_OS_UNIX ) || defined( HB_OS_DOS )
 
 #define MAX_CHAR_VAL  0xff
 #define HB_CHRMAP( a, c )  ( ( ( a ) << 16 ) | ( c ) )