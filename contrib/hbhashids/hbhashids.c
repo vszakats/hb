07c9002455e5dfad2a4225e73034ac3d694ee754
Viktor Szakats <vszakats@users.noreply.github.com>
2016-11-02 16:31:32 +0100

    apply multiple fixes for ANSI C, type, buffer and general cleanup
    (all untested)

--- contrib/hbhashids/hbhashids.c
+++ contrib/hbhashids/hbhashids.c
@@ -1,55 +1,51 @@
-/*
+/* libhashids wrapper */
 
-   libhashids wrapper
+#include "hashids.h"
 
- */
+#include "hbapi.h"
+#include "hbapierr.h"
+#include "hbapiitm.h"
 
-#include "hbhashids.h"
-
-#define ULONGLONG    unsigned long long
 #define MAX_NUMBERS  256
 
-// HASHIDS_ENCODE( aNumbers | nNumber, [cSalt], [nMinHashLength], [cAlphabet] )
+/* hashids_encode( aNumbers | nNumber, [cSalt], [nMinHashLength], [cAlphabet] ) */
 HB_FUNC( HASHIDS_ENCODE )
 {
    if( HB_ISARRAY( 1 ) || HB_ISNUM( 1 ) )
    {
       const char * salt = HB_ISCHAR( 2 ) ? hb_parc( 2 ) : HASHIDS_DEFAULT_SALT;
-      size_t       minHashLength = HB_ISNUM( 3 ) ? hb_parni( 3 ) : 0;
+      HB_SIZE      minHashLength = hb_parns( 3 );
       const char * alphabet      = HB_ISCHAR( 4 ) ? hb_parc( 4 ) : HASHIDS_DEFAULT_ALPHABET;
 
-      hashids_t * pHash = hashids_init3( salt, minHashLength, alphabet );
+      hashids_t * pHash = hashids_init3( salt, ( size_t ) minHashLength, alphabet );
 
       if( pHash )
       {
-         size_t numbers_count;
-
-         if( HB_ISNUM( 1 ) )
-            numbers_count = 1;
-         else       // array
-            numbers_count = hb_arrayLen( hb_param( 1, HB_IT_ARRAY ) );
-
-         ULONGLONG numbers[ numbers_count ];
+         HB_SIZE numbers_count;
+         HB_ULONGLONG numbers[ MAX_NUMBERS ];
+         char * pBuffer;
+         HB_SIZE bytes_encoded;
 
          if( HB_ISNUM( 1 ) )
          {
-            numbers[ 0 ] = ( ULONGLONG ) hb_parnll( 1 );
+            numbers_count = 1;
+            numbers[ 0 ] = ( HB_ULONGLONG ) hb_parnll( 1 );
          }
-         else       // array
+         else  /* array */
          {
             PHB_ITEM pArray = hb_param( 1, HB_IT_ARRAY );
 
+            numbers_count = HB_MIN( hb_arrayLen( hb_param( 1, HB_IT_ARRAY ) ), HB_SIZEOFARRAY( numbers ) );
+
             for( size_t i = 0; i < numbers_count; ++i )
-               numbers[ i ] = ( ULONGLONG ) hb_arrayGetNLL( pArray, i + 1 );
+               numbers[ i ] = ( HB_ULONGLONG ) hb_arrayGetNLL( pArray, i + 1 );
          }
 
-         char * pBuffer = ( char * ) hb_xgrabz( hashids_estimate_encoded_size( pHash, numbers_count, numbers ) );
+         pBuffer = ( char * ) hb_xgrabz( hashids_estimate_encoded_size( pHash, ( size_t ) numbers_count, numbers ) + 1 );
+         bytes_encoded = hashids_encode( pHash, pBuffer, ( size_t ) numbers_count, numbers );
 
-         size_t bytes_encoded = hashids_encode( pHash, pBuffer, numbers_count, numbers );
+         hb_retclen_buffer( pBuffer, bytes_encoded );
 
-         hb_retclen( pBuffer, bytes_encoded );
-
-         hb_xfree( pBuffer );
          hashids_free( pHash );
       }
       else
@@ -59,24 +55,21 @@ HB_FUNC( HASHIDS_ENCODE )
       hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
 }
 
-// HASHIDS_DECODE( cEncoded, [cSalt], [nMinHashLength], [cAlphabet] ) -> aNumbers
+/* hashids_decode( cEncoded, [cSalt], [nMinHashLength], [cAlphabet] ) -> aNumbers */
 HB_FUNC( HASHIDS_DECODE )
 {
    if( HB_ISCHAR( 1 ) )
    {
       const char * salt = HB_ISCHAR( 2 ) ? hb_parc( 2 ) : HASHIDS_DEFAULT_SALT;
-      size_t       minHashLength = HB_ISNUM( 3 ) ? hb_parni( 3 ) : 0;
+      HB_SIZE      minHashLength = hb_parns( 3 );
       const char * alphabet      = HB_ISCHAR( 4 ) ? hb_parc( 4 ) : HASHIDS_DEFAULT_ALPHABET;
 
-      hashids_t * pHash = hashids_init3( salt, minHashLength, alphabet );
+      hashids_t * pHash = hashids_init3( salt, ( size_t ) minHashLength, alphabet );
 
       if( pHash )
       {
-
-         ULONGLONG numbers[ MAX_NUMBERS ];
-
-         size_t numbers_count = hashids_decode( pHash, ( char * ) hb_parc( 1 ), numbers );
-
+         HB_ULONGLONG numbers[ MAX_NUMBERS ];
+         HB_SIZE numbers_count = ( HB_SIZE ) hashids_decode( pHash, ( char * ) hb_parc( 1 ), numbers );
          PHB_ITEM pArray = hb_itemArrayNew( numbers_count );
 
          for( size_t i = 0; i < numbers_count; i++ )
@@ -91,5 +84,4 @@ HB_FUNC( HASHIDS_DECODE )
    }
    else
       hb_errRT_BASE( EG_ARG, 3012, NULL, HB_ERR_FUNCNAME, HB_ERR_ARGS_BASEPARAMS );
-
 }
