d1665a1aa04a83cbf0577e07e230e26fbf3c220a
Viktor Szakats <vszakats@users.noreply.github.com>
2017-02-01 20:24:34 +0000

    2017-02-01 20:23 UTC Viktor Szakats (vszakats users.noreply.github.com)   * .editorconfig   * .gitattributes     ! updates and fixes
      * .gitattributes
        ! resync with reality
    
      * contrib/hbwin/tests/oletst2.js
      * contrib/xhb/tests/test.xml
        * indentation

--- contrib/hbwin/tests/oletst2.js
+++ contrib/hbwin/tests/oletst2.js
@@ -1,6 +1,6 @@
 /* Copyright 2010 Viktor Szakats (vszakats.net/harbour) */
 {
-   var tst2 = new ActiveXObject("MyOleTimeServer");
+  var tst2 = new ActiveXObject("MyOleTimeServer");
 
-   WScript.CreateObject("Wscript.Shell").Popup(">" + tst2.TIME() + "<");
+  WScript.CreateObject("Wscript.Shell").Popup(">" + tst2.TIME() + "<");
 }
