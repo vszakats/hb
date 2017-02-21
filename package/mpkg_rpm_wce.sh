35e3cbe91287b8d503b64f52e79303001df57021
Viktor Szakats <vszakats@users.noreply.github.com>
2017-02-21 21:22:02 +0000

    2017-02-21 21:20 UTC Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * package/mpkg_rpm_wce.sh   * package/mpkg_rpm_win.sh   * package/mpkg_rpm.sh   * package/mpkg_src.sh     * reindent with industry standard 2 spaces     * tweak quote chars to make buggy syntax highlighter happy (mcedit)

--- package/mpkg_rpm_wce.sh
+++ package/mpkg_rpm_wce.sh
@@ -9,23 +9,23 @@
 
 test_reqrpm()
 {
-   rpm -q --whatprovides "$1" >/dev/null 2>&1
+  rpm -q --whatprovides "$1" >/dev/null 2>&1
 }
 
 get_rpmmacro()
 {
-   _R=$(rpm --showrc|sed -e "/^-14:.${1}[^a-z0-9A-Z_]/ !d" -e "s/^-14: ${1}.//")
-   _X=$(echo "${_R}"|sed -e "s/.*\(%{\([^}]*\)}\).*/\2/")
-   while [ "${_X}" != "${_R}" ]; do
-      _Y=$(get_rpmmacro "$_X")
-      if [ -n "${_Y}" ]; then
-         _R=$(echo "${_R}"|sed -e "s!%{${_X}}!${_Y}!g")
-         _X=$(echo "${_R}"|sed -e "s/.*\(%{\([^}]*\)}\).*/\2/")
-      else
-         _X="${_R}"
-      fi
-   done
-   printf %s "${_R}"
+  _R="$(rpm --showrc | sed -e "/^-14:.${1}[^a-z0-9A-Z_]/ !d" -e "s/^-14: ${1}.//")"
+  _X="$(echo "${_R}" | sed -e 's/.*\(%{\([^}]*\)}\).*/\2/')"
+  while [ "${_X}" != "${_R}" ]; do
+    _Y=$(get_rpmmacro "$_X")
+    if [ -n "${_Y}" ]; then
+      _R="$(echo "${_R}" | sed -e "s!%{${_X}}!${_Y}!g")"
+      _X="$(echo "${_R}" | sed -e 's/.*\(%{\([^}]*\)}\).*/\2/')"
+    else
+      _X="${_R}"
+    fi
+  done
+  printf %s "${_R}"
 }
 
 cd "$(dirname "$0")" || exit
@@ -39,54 +39,54 @@ NEED_RPM='make gcc binutils cegcc-mingw32ce'
 FORCE=''
 
 while [ $# -gt 0 ]; do
-   if [ "$1" = '--force' ]; then
-      FORCE='yes'
-   else
-      INST_PARAM="${INST_PARAM} $1"
-   fi
-   shift
+  if [ "$1" = '--force' ]; then
+    FORCE='yes'
+  else
+    INST_PARAM="${INST_PARAM} $1"
+  fi
+  shift
 done
 
 TOINST_LST=''
 for i in ${NEED_RPM}; do
-   test_reqrpm "$i" || TOINST_LST="${TOINST_LST} $i"
+  test_reqrpm "$i" || TOINST_LST="${TOINST_LST} $i"
 done
 
 if [ -z "${TOINST_LST}" ] || [ "${FORCE}" = 'yes' ]; then
-   . ./mpkg_src.sh
-   stat="$?"
-   if [ -z "${hb_filename}" ]; then
-      echo "The script ./mpkg_src.sh doesn't set archive name to \${hb_filename}"
-      exit 1
-   elif [ "${stat}" != 0 ]; then
-      echo 'Error during packing the sources in ./mpkg_src.sh'
-      exit 1
-   elif [ -f "${hb_filename}" ]; then
-      if [ "$(id -u)" != 0 ] && [ ! -f "${HOME}/.rpmmacros" ]; then
-         RPMDIR="${HOME}/RPM"
-         mkdir -p "${RPMDIR}/SOURCES" "${RPMDIR}/RPMS" "${RPMDIR}/SRPMS" \
-                  "${RPMDIR}/BUILD" "${RPMDIR}/SPECS"
-         echo "%_topdir ${RPMDIR}" > "${HOME}/.rpmmacros"
-      else
-         RPMDIR=$(get_rpmmacro '_topdir')
-      fi
+  . ./mpkg_src.sh
+  stat="$?"
+  if [ -z "${hb_filename}" ]; then
+    echo "The script ./mpkg_src.sh doesn't set archive name to \${hb_filename}"
+    exit 1
+  elif [ "${stat}" != 0 ]; then
+    echo 'Error during packing the sources in ./mpkg_src.sh'
+    exit 1
+  elif [ -f "${hb_filename}" ]; then
+    if [ "$(id -u)" != 0 ] && [ ! -f "${HOME}/.rpmmacros" ]; then
+      RPMDIR="${HOME}/RPM"
+      mkdir -p "${RPMDIR}/SOURCES" "${RPMDIR}/RPMS" "${RPMDIR}/SRPMS" \
+               "${RPMDIR}/BUILD" "${RPMDIR}/SPECS"
+      echo "%_topdir ${RPMDIR}" > "${HOME}/.rpmmacros"
+    else
+      RPMDIR=$(get_rpmmacro '_topdir')
+    fi
 
-      mv "${hb_filename}" "${RPMDIR}/SOURCES/"
-      sed -e "s/^%define version .*$/%define version   ${hb_ver}/g" \
-          -e "s/^%define releasen .*$/%define releasen  ${hb_verstat}/g" \
-         harbour-wce.spec.in > "${RPMDIR}/SPECS/harbour-wce.spec"
+    mv "${hb_filename}" "${RPMDIR}/SOURCES/"
+    sed -e "s/^%define version .*$/%define version   ${hb_ver}/g" \
+        -e "s/^%define releasen .*$/%define releasen  ${hb_verstat}/g" \
+        harbour-wce.spec.in > "${RPMDIR}/SPECS/harbour-wce.spec"
 
-      cd "${RPMDIR}/SPECS" || exit
+    cd "${RPMDIR}/SPECS" || exit
 
-      # shellcheck disable=SC2086
-      rpmbuild -ba harbour-wce.spec ${INST_PARAM}
-   else
-      echo "Cannot find archive file: ${hb_filename}"
-      exit 1
-   fi
+    # shellcheck disable=SC2086
+    rpmbuild -ba harbour-wce.spec ${INST_PARAM}
+  else
+    echo "Cannot find archive file: ${hb_filename}"
+    exit 1
+  fi
 else
-   echo 'If you want to build Harbour compiler'
-   echo 'you have to install the folowing RPM files:'
-   echo "${TOINST_LST}"
-   exit 1
+  echo 'If you want to build Harbour compiler'
+  echo 'you have to install the folowing RPM files:'
+  echo "${TOINST_LST}"
+  exit 1
 fi
--- package/mpkg_src.sh
+++ package/mpkg_src.sh
@@ -3,15 +3,15 @@
 # This script requires 'GNU tar' tool for compression.
 
 if [ "$1" = 'zip' ] || [ "$1" = 'ZIP' ]; then
-   hb_archbin='zip'
-   hb_ext='.zip'
+  hb_archbin='zip'
+  hb_ext='.zip'
 elif tar --version >/dev/null 2>&1; then
-   hb_archbin='tar'
+  hb_archbin='tar'
 elif gtar --version >/dev/null 2>&1; then
-   hb_archbin='gtar'
+  hb_archbin='gtar'
 else
-   hb_archbin='tar'
-   echo "Warning!!! Cannot find 'GNU tar'"
+  hb_archbin='tar'
+  echo "Warning!!! Cannot find 'GNU tar'"
 fi
 
 hb_currdir="$(pwd)"
@@ -20,10 +20,10 @@ hb_archopt='-czf'
 [ -n "${hb_ext}" ] || hb_ext='.tar.gz'
 
 if [ -f mpkg_ver.sh ]; then
-   hb_rootdir='..'
+  hb_rootdir='..'
 else
-   hb_rootdir=$(dirname "$0")
-   hb_rootdir=$(dirname "${hb_rootdir}")
+  hb_rootdir=$(dirname "$0")
+  hb_rootdir=$(dirname "${hb_rootdir}")
 fi
 # shellcheck source=./mpkg_ver.sh
 . "${hb_rootdir}/package/mpkg_ver.sh"
@@ -36,40 +36,40 @@ rm -f "$hb_filename"
 
 hb_collect_all_git()
 {
-   for f in $(git ls-tree HEAD -r --name-only); do
-      [ -f "$f" ] && echo "$f"
-   done
+  for f in $(git ls-tree HEAD -r --name-only); do
+    [ -f "$f" ] && echo "$f"
+  done
 }
 
 hb_collect_all_tree()
 {
-   unset GREP_OPTIONS
-   _exclude='/obj/|/lib/|/bin/.*/|\.tar|\.zip|\.exe|\.log|/linux/|/win|/config/'
-   for f in $(find . -type f | grep -vE "${_exclude}"); do
-      echo "$f" | awk '{ string=substr($0, 2); print string; }'
-   done
-   find config -type f -exec echo '{}' \;
+  unset GREP_OPTIONS
+  _exclude='/obj/|/lib/|/bin/.*/|\.tar|\.zip|\.exe|\.log|/linux/|/win|/config/'
+  for f in $(find . -type f | grep -vE "${_exclude}"); do
+    echo "$f" | awk '{ string=substr($0, 2); print string; }'
+  done
+  find config -type f -exec echo '{}' \;
 }
 
 hb_rmflst='yes'
 hb_flst='bin/hb_flst.tmp'
 (
-   cd "$hb_rootdir" || exit
-   if [ -d '.git' ]; then
-      hb_collect_all_git
-      echo "$hb_flst"
-   else
-      hb_collect_all_tree
-   fi
+  cd "$hb_rootdir" || exit
+  if [ -d '.git' ]; then
+    hb_collect_all_git
+    echo "$hb_flst"
+  else
+    hb_collect_all_tree
+  fi
 ) > "$hb_rootdir/$hb_flst"
 
 (
-   cd "$hb_rootdir" || exit
-   if [ "$hb_archbin" = 'zip' ]; then
-      $hb_archbin -r -q "$hb_filename" . "-i@$hb_flst"
-   else
-      $hb_archbin $hb_archopt "$hb_filename" --files-from "$hb_flst"
-   fi
+  cd "$hb_rootdir" || exit
+  if [ "$hb_archbin" = 'zip' ]; then
+    $hb_archbin -r -q "$hb_filename" . "-i@$hb_flst"
+  else
+    $hb_archbin $hb_archopt "$hb_filename" --files-from "$hb_flst"
+  fi
 )
 [ "$hb_rmflst" != 'yes' ] || rm -fR "${hb_rootdir:?}/$hb_flst"
 
