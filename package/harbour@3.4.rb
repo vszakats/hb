harbour/core

commit 888945a35e4086fa307bb213e42ab666ad789d32

2014-03-14 22:56 UTC+0100 Viktor Szakats (vszakats users.noreply.github.com)   * contrib/hbssl/readme.txt     + added *nix package names
  * contrib/sddmy/readme.txt
    + added deb/rpm mariadb package names

  * debian/control
    * sync with contribs
    * moved core dependencies to top

debian/control
@@ -2,13 +2,15 @@ Source: harbour
 Section: devel
 Priority: optional
 Maintainer: Luis Mayoral <mayoral@linuxadicto.org>
-Build-Depends: debhelper (>= 5.0.0), libjpeg62-dev, libpcre3-dev,
- libpng12-dev, zlib1g-dev, libsqlite3-dev, liballegro4.2-dev, libbz2-dev,
+Build-Depends: debhelper (>= 5.0.0),
+ libjpeg62-dev, libpcre3-dev, libpng12-dev, zlib1g-dev,
+ libslang2-dev, libncurses5-dev | libncursesw5-dev, libslang2-dev, libx11-dev,
+ libsqlite3-dev, liballegro4.2-dev, libbz2-dev,
  libcairo2-dev, libcups2-dev, libexpat1-dev, libfreeimage-dev,
  libcurl4-openssl-dev | libcurl4-gnutls-dev, firebird2.0-dev | firebird2.1-dev,
- libgd2-noxpm-dev | libgd2-xpm-dev, libmagic-dev, libmysqlclient-dev,
- unixodbc-dev, libpq-dev, libssl-dev,
- libslang2-dev, libncurses5-dev | libncursesw5-dev, libslang2-dev, libx11-dev
+ libgd2-noxpm-dev | libgd2-xpm-dev, libmagic-dev, libmariadbd-dev | libmysqlclient-dev,
+ unixodbc-dev, libpq-dev, libssl-dev
+
 Standards-Version: 3.6.2
 
 Package: harbour

vszakats/hb

commit bf260831454f24c5decbfae6e823f13780c01b92

2017-03-11 03:06 UTC Viktor Szakats (vszakats users.noreply.github.com)   * .travis.yml     * remove wine debug
  * debian/control
    ! try fixing error:
      `dpkg-shlibdeps: error: syntax error in debian/control at line 25: block lacks the 'Package' field`
      Possibly a regression after 888945a35e4086fa307bb213e42ab666ad789d32
      Ref: https://groups.google.com/d/msg/harbour-users/2KMRVd_im9c/jhVywEOrAwAJ

  * README.md
  * package/harbour.rb -> package/harbour@3.4.rb
    * rename Homebrew package

  * package/mpkg_win_dl_mxe.sh
    * rename variables

  * src/common/expropt2.c
    * whitespace

  * utils/hbmk2/hbmk2.prg
    * revert the following modification from f5faad1c9c34904b93101d15a5a634ac5c8daed8:
        % optimize script runner to load the script from disk just once
      Since the script filename was not known after this change, the
      runtime errors reported __hbInit() as the source filename and
      compile-time errors used `{SOURCE}`, which is a bit confusing.
      The related PP fix in 2017-03-03 10:15 UTC stays committed though.

  * utils/hbtest/rt_array.prg
    ! fix test that gave wrong result when hbtest was run as script.

  * utils/hbtest/rt_hvm.prg
  * utils/hbtest/rt_str.prg
    + add tests for ARM double to ingeter conversion issues
      Ref: https://github.com/harbour/core/issues/141

.travis.yml
@@ -85,10 +85,7 @@ script:
   - if [ "$CC" = 'i686-w64-mingw32-gcc' ]; then
       if [ "$TRAVIS_OS_NAME" = 'osx' ]; then
         export PATH="/Applications/Wine Devel.app/Contents/Resources/wine/bin:${PATH}";
-        brew list;
-        date;
         export DISPLAY=:88.0; wineboot;
-        date;
       fi;
       if ./package/mpkg_win_ci.sh && [ "${TRAVIS_BRANCH#*prod*}" = "${TRAVIS_BRANCH}" ] ; then
         mv -f harbour-snapshot-win.7z "harbour-snapshot-win-built-on-${TRAVIS_OS_NAME}.7z";

debian/control
@@ -20,7 +20,6 @@ Build-Depends: debhelper (>= 5.0.0),
  libsqlite3-dev,
  libssl-dev,
  unixodbc-dev
-
 Standards-Version: 3.6.2
 
 Package: harbour

package/harbour@3.4.rb
@@ -1,4 +1,4 @@
-class Harbour < Formula
+class HarbourAT34 < Formula
   desc "Portable, xBase-compatible programming language and environment"
   homepage "https://github.com/vszakats/harbour-core/"
 

package/mpkg_win_dl_mxe.sh
@@ -27,15 +27,15 @@ mxe_get_pkg() {
       if [[ ! "${done}" = *"|${idid}|"* ]]; then  # avoid installing the same package twice
         done="${done} |${idid}|"  # add to list of install packages
 
-        sect="$(awk "/^Package: ${repo}-${name}$/,/^SHA256: /" Packages)"  # control section for this package
+        ctrl="$(awk "/^Package: ${repo}-${name}$/,/^SHA256: /" Packages)"  # control section for this package
 
-        debu="$(echo "${sect}" | sed -n -E 's,^Filename: (.+)$,\1,p')"  # .deb path
-        vers="$(echo "${sect}" | sed -n -E 's,^Version: (.+)$,\1,p')"  # package version
-        hash="$(echo "${sect}" | sed -n -E 's,^SHA256: ([0-9a-fA-F]{64})$,\1,p')"  # .deb hash
-        dept="$(echo "${sect}" | sed -n -E 's,^Depends: (.+)$,\1,p')"  # .deb dependencies
+        debp="$(echo "${ctrl}" | sed -n -E 's,^Filename: (.+)$,\1,p')"  # .deb path
+        vers="$(echo "${ctrl}" | sed -n -E 's,^Version: (.+)$,\1,p')"  # package version
+        hash="$(echo "${ctrl}" | sed -n -E 's,^SHA256: ([0-9a-fA-F]{64})$,\1,p')"  # .deb hash
+        deps="$(echo "${ctrl}" | sed -n -E 's,^Depends: (.+)$,\1,p')"  # .deb dependencies
 
         echo "! Version: ${vers}"
-        url="${base}/${debu}"
+        url="${base}/${debp}"
         echo "! Downloading... '${url}'"
         if curl -fsS "${url}" -o pack.bin; then
 
@@ -58,7 +58,7 @@ mxe_get_pkg() {
           fi
           rm -f pack.bin
 
-          for i in ${dept//,/}; do
+          for i in ${deps//,/}; do
             mxe_get_pkg "$i"  # recurse
           done
         else

src/common/expropt2.c
@@ -2041,7 +2041,7 @@ HB_BOOL hb_compExprReduceCHR( PHB_EXPR pSelf, HB_COMP_DECL )
       /* NOTE: CA-Cl*pper's compiler optimizer will be wrong for those
        *       Chr() cases where the passed parameter is a constant which
        *       can be divided by 256 but it's not zero, in this case it
-       *       will return an empty string instead of a Chr(0). [vszakats]
+       *       will return an empty string instead of a Chr( 0 ). [vszakats]
        *
        *       But this bug exist only in compiler and CA-Cl*pper macro
        *       compiler does not have optimizer. This bug is replicated

utils/hbmk2/hbmk2.prg
@@ -15976,7 +15976,7 @@ STATIC PROCEDURE __hbshell( cFile, ... )
                     we use the same <comp> values as was used to build itself.)
           */
 
-         __hbshell_LoadExtFromSource( aExtension, cFile := hbmk_MemoRead( cFile ) )
+         __hbshell_LoadExtFromSource( aExtension, cFile )
 
          /* NOTE: Find .hbc file. Load .hbc file. Process .hbc references.
                   Pick include paths. Load libs. Add include paths to include
@@ -16008,13 +16008,13 @@ STATIC PROCEDURE __hbshell( cFile, ... )
          NEXT
 
          /* We can use this function as this is a GPL licenced application */
-         cFile := hb_compileFromBuf( ;
-            cFile, ;
+         cFile := hb_compileBuf( ;
             hbmk_CoreHeaderFiles(), ;
             hb_ProgName(), ;
             "-n2", "-w", "-es2", "-q0", ;
             hb_ArrayToParams( aOPTPRG ), ;
-            "-D" + _HBMK_SHELL )
+            "-D" + _HBMK_SHELL, ;
+            cFile )
 
          IF cFile == NIL
             ErrorLevel( _EXIT_COMPPRG )
@@ -16107,8 +16107,9 @@ STATIC PROCEDURE __hbshell_LoadExtFromString( aExtension, cString )
 
    RETURN
 
-STATIC PROCEDURE __hbshell_LoadExtFromSource( aExtension, cFile )
+STATIC PROCEDURE __hbshell_LoadExtFromSource( aExtension, cFileName )
 
+   LOCAL cFile := hbmk_MemoRead( cFileName )
    LOCAL pRegex
    LOCAL tmp
 

utils/hbtest/rt_array.prg
@@ -212,14 +212,15 @@ PROCEDURE Main_ARRAY()
    HBTEST Array( 5000 )                   IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 "
 #endif
    HBTEST Array( 1 )                      IS "{.[1].}"
-   HBTEST Array( -1 )                     IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 "
 #ifdef __HARBOUR__
    /* disable Harbour extended optimizations to test correct RTE message */
    #pragma -ko-
 #endif
 #ifdef HB_CLP_STRICT
+   HBTEST Array( -1 )                     IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 "
    HBTEST Array( 1, 0, -10 )              IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 "
 #else
+   HBTEST Array( -1 )                     IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 A:1:N:-1 "
    HBTEST Array( 1, 0, -10 )              IS "E 2 BASE 1131 Bound error (array dimension) OS:0 #:0 A:3:N:1;N:0;N:-10 "
 #endif
    HBTEST Array( 1, 0, "A" )              IS NIL

utils/hbtest/rt_hvm.prg
@@ -46,6 +46,44 @@
 
 PROCEDURE Main_HVM()
 
+   LOCAL dNegHigh := -100000.0
+   LOCAL iNegHigh := -100000
+
+   /* Internal double to integer conversions */
+
+   HBTEST Asc( Chr( -100000 ) )              IS 96
+   HBTEST Asc( Chr( -100000.0 ) )            IS 96
+   HBTEST Asc( Chr( iNegHigh ) )             IS 96
+   HBTEST Asc( Chr( dNegHigh ) )             IS 96
+#ifdef __HARBOUR__
+   HBTEST Asc( hb_BChar( -100000 ) )         IS 96
+   HBTEST Asc( hb_BChar( -100000.0 ) )       IS 96
+   HBTEST Asc( hb_BChar( iNegHigh ) )        IS 96
+   HBTEST Asc( hb_BChar( dNegHigh ) )        IS 96
+   HBTEST hb_SToD( "20170310" ) - -100000    IS hb_SToD( "22901224" )
+   HBTEST hb_SToD( "20170310" ) - -100000.0  IS hb_SToD( "22901224" )
+#endif
+   HBTEST hb_SToD( "20170310" ) + -100000    IS hb_SToD( "17430526" )
+   HBTEST hb_SToD( "20170310" ) + -100000.0  IS hb_SToD( "17430526" )
+   HBTEST -100000   + hb_SToD( "20170310" )  IS hb_SToD( "17430526" )
+   HBTEST -100000.0 + hb_SToD( "20170310" )  IS hb_SToD( "17430526" )
+#ifdef __HARBOUR__
+   HBTEST hb_SToD( "20170310" ) - iNegHigh   IS hb_SToD( "22901224" )
+   HBTEST hb_SToD( "20170310" ) - dNegHigh   IS hb_SToD( "22901224" )
+#endif
+   HBTEST hb_SToD( "20170310" ) + iNegHigh   IS hb_SToD( "17430526" )
+   HBTEST hb_SToD( "20170310" ) + dNegHigh   IS hb_SToD( "17430526" )
+   HBTEST iNegHigh + hb_SToD( "20170310" )   IS hb_SToD( "17430526" )
+   HBTEST dNegHigh + hb_SToD( "20170310" )   IS hb_SToD( "17430526" )
+
+   /* Internal date value overflow */
+
+#if 0
+   /* Explanation: https://github.com/harbour/core/issues/142 */
+   HBTEST          hb_SToD( "20170310" ) + 10000000   IS          hb_SToD( "93960404" )
+   HBTEST ValType( hb_SToD( "20170310" ) + 10000000 ) IS ValType( hb_SToD( "93960404" ) )
+#endif
+
    /* ValType() */
 
    HBTEST ValType(  scString  )           IS "C"

utils/hbtest/rt_str.prg
@@ -177,6 +177,7 @@ PROCEDURE Main_STR()
    HBTEST Chr( "A" )                      IS "E 1 BASE 1104 Argument error (CHR) OS:0 #:0 A:1:C:A F:S"
    HBTEST Chr( "ADDDDDD" )                IS "E 1 BASE 1104 Argument error (CHR) OS:0 #:0 A:1:C:ADDDDDD F:S"
    HBTEST Chr( -10000000.0 )              IS Chr( 128 )
+   HBTEST Chr( -10000000 )                IS Chr( 128 )
    HBTEST Chr( -100000 )                  IS Chr( 96 )
    HBTEST Chr( -65 )                      IS Chr( 191 )
    HBTEST Chr( snIntP1 )                  IS "A"