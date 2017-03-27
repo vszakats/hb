05045972a3ac833c98de5f6f4f1ff7c762b254dc
Viktor Szakats <vszakats@users.noreply.github.com>
2017-03-27 10:45:07 +0000

    2017-03-27 10:42 UTC Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * contrib/hbcups/hbcups.hbp     * add another valid rpm cups package name
      * package/mpkg_rpm.sh
        * wrap long lines
        * sync package names with contrib '{rpm}' lines
        + detect ghostscript and libmagic
    
      * package/harbour.spec
        * replace deprecated backtick syntax

--- package/mpkg_rpm.sh
+++ package/mpkg_rpm.sh
@@ -41,29 +41,46 @@ if [ "$HB_WITH_ADS" != 'no' ]; then
     INST_PARAM="${INST_PARAM} --with ads"
   fi
 fi
-if test_reqrpm 'cairo-devel' && [ "$HB_WITH_CAIRO" != 'no' ]; then
+if test_reqrpm 'cairo-devel' && \
+   [ "$HB_WITH_CAIRO" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with cairo"
 fi
-if test_reqrpm 'libcups2-devel' && [ "$HB_WITH_CUPS" != 'no' ]; then
+if ( test_reqrpm 'libcups2-devel' || \
+     test_reqrpm 'cups-devel' ) && \
+   [ "$HB_WITH_CUPS" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with cups"
 fi
-if test_reqrpm 'curl-devel' && [ "$HB_WITH_CURL" != 'no' ]; then
+if test_reqrpm 'curl-devel' && \
+   [ "$HB_WITH_CURL" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with curl"
 fi
-if test_reqrpm 'openssl' && [ "$HB_WITH_OPENSSL" != 'no' ]; then
+if ( test_reqrpm 'openssl' || \
+     test_reqrpm 'openssl-devel' ) && \
+   [ "$HB_WITH_OPENSSL" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with openssl"
 fi
-if test_reqrpm 'firebird-devel' && [ "$HB_WITH_FIREBIRD" != 'no' ]; then
+if test_reqrpm 'firebird-devel' && \
+   [ "$HB_WITH_FIREBIRD" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with firebird"
 fi
-if test_reqrpm 'freeimage-devel' && [ "$HB_WITH_FREEIMAGE" != 'no' ]; then
+if test_reqrpm 'freeimage-devel' && \
+   [ "$HB_WITH_FREEIMAGE" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with freeimage"
 fi
-if test_reqrpm 'gd-devel' && [ "$HB_WITH_GD" != 'no' ]; then
+if test_reqrpm 'gd-devel' && \
+   [ "$HB_WITH_GD" != 'no' ]; then
   v="$(rpm -q --whatprovides gd-devel --qf "%{VERSION}" | sed -e 's/[^0-9]*\([0-9]*\).*/\1/g')"
   [ "$v" -ge 2 ] && INST_PARAM="${INST_PARAM} --with gd"
 fi
-if ( test_reqrpm 'libmariadbd-devel' || \
+if test_reqrpm 'ghostscript-devel' && \
+   [ "$HB_WITH_GS" != 'no' ]; then
+  INST_PARAM="${INST_PARAM} --with gs"
+fi
+if test_reqrpm 'file-libs' && \
+   [ "$HB_WITH_LIBMAGIC" != 'no' ]; then
+  INST_PARAM="${INST_PARAM} --with libmagic"
+fi
+if ( test_reqrpm 'libmariadb-devel' || \
      test_reqrpm 'libmysqlclient-devel' || \
      test_reqrpm 'MySQL-devel' || \
      test_reqrpm 'mysql-devel' ) && \
@@ -75,7 +92,8 @@ if ( test_reqrpm 'unixodbc-devel' || \
    [ "$HB_WITH_ODBC" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with odbc"
 fi
-if test_reqrpm 'postgresql-devel' && [ "$HB_WITH_PGSQL" != 'no' ]; then
+if test_reqrpm 'postgresql-devel' && \
+   [ "$HB_WITH_PGSQL" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --with pgsql"
 fi
 
@@ -87,16 +105,17 @@ if [ "${HB_BUILD_NOGPLLIB}" = 'yes' ] || \
    ! test_reqrpm 'gpm-devel'; then
   INST_PARAM="${INST_PARAM} --without gpm"
 fi
-if ! test_reqrpm 'XFree86-devel' && [ "$HB_WITH_X11" != 'no' ]; then
+if ! test_reqrpm 'XFree86-devel' && \
+   [ "$HB_WITH_X11" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --without X11"
 fi
-if ! test_reqrpm ncurses || \
-   ! test_reqrpm ncurses-devel || \
+if ! test_reqrpm 'ncurses' || \
+   ! test_reqrpm 'ncurses-devel' || \
    [ "$HB_WITH_CURSES" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --without curses"
 fi
-if ! test_reqrpm slang || \
-   ! test_reqrpm slang-devel || \
+if ! test_reqrpm 'slang' || \
+   ! test_reqrpm 'slang-devel' || \
    [ "$HB_WITH_SLANG" != 'no' ]; then
   INST_PARAM="${INST_PARAM} --without slang"
 fi
@@ -115,7 +134,8 @@ if ( [ ! -f /usr/include/pcre2.h ] && \
    [ "$HB_WITH_PCRE2" = 'local' ]; then
   INST_PARAM="${INST_PARAM} --with localpcre2"
 fi
-if ( [ ! -f /usr/include/bzlib.h ] && [ ! -f /usr/local/include/bzlib.h ] ) || \
+if ( [ ! -f /usr/include/bzlib.h ] && \
+     [ ! -f /usr/local/include/bzlib.h ] ) || \
   [ "$HB_WITH_BZIP2" = "local" ]
 then
   INST_PARAM="${INST_PARAM} --with localbz2"
