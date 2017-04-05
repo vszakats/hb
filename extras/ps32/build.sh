69a2831b273fa5088594827eb8fe20f56de676c8
Viktor Szakats <vszakats@users.noreply.github.com>
2017-04-05 17:50:47 +0000

    2017-04-05 17:46 UTC Viktor Szakats (vszakats users.noreply.github.com)   - extras/ps32/tests/hbmk.hbm     * cleanup
      + extras/ps32/build.sh
        + add shell script to execute all download/patch/build steps
          at once. Also build a test app.
          Fix few issues along the way.
    
      * extras/ps32/ps32.patch
        ! fix a function that missed a `return` statement
        ! fix #includes to use lowercase filenames and no harcoded paths
          for portability
    
      * extras/ps32/ps32.hbp
        * be silent
    
      * extras/superlib/build.sh
        * be more silent
        * cleanup
    
      - extras/ps32/README.md
      - extras/superlib/README.md
        % remove READMEs in favour of POSIX shell scripts
          (easily adaptable for MS-DOS batch environments)

---
+++ extras/ps32/build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+curl -LO --proto-redir =https https://web.archive.org/web/abeelabs.com/downloads/PS32.zip
+openssl dgst -sha256 PS32.zip | grep -q 'c9a9fb6d81054468d2884a913275184a318be8a5c830bc901376ae88e720849d' || exit 1
+
+unzip -qo PS32.zip
+
+cp 'PS32/xHarbour/PScript.ch' .
+cp 'PS32/xHarbour/TPSCRIPT.PRG' .
+cp 'PS32/Demos/xbase sources/PSTest.prg' .
+
+hbmk2 -sanitize *.ch *.prg
+
+patch -li ps32.patch
+
+hbmk2 ps32.hbp
+hbmk2 pstest.prg ps32.hbc
--- extras/superlib/build.sh
+++ extras/superlib/build.sh
@@ -3,13 +3,13 @@
 curl -O https://harbour.github.io/the-oasis/files/library/supfree.zip
 openssl dgst -sha256 supfree.zip | grep -q '48114a59c8a9ebf51749f74e31dac39e12403180ae2920afb9f93d82794a8fc6' || exit 1
 
-unzip supfree.zip
-unzip SOURCE.ZIP
+unzip -qo supfree.zip
+unzip -qo SOURCE.ZIP
 
-hbmk2 -sanitize '*.C' '*.PRG'
-patch -lNi superlib.patch
+hbmk2 -sanitize *.C *.PRG
+patch -li superlib.patch
 
 hbmk2 superlib.hbp
 
-unzip SAMPLES.ZIP
+unzip -qo SAMPLES.ZIP
 hbmk2 S3PROG.PRG superlib.hbc -run
