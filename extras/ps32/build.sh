75f85b84fde6b8720ca74065eff8a141978d4f9f
Viktor Szakats <vszakats@users.noreply.github.com>
2017-04-06 10:12:44 +0000

    2017-04-06 10:11 UTC Viktor Szakats (vszakats users.noreply.github.com)   * extras/ps32/build.sh   * extras/superlib/build.sh     % refinements: less trashing on local disk and more universal commands

--- extras/ps32/build.sh
+++ extras/ps32/build.sh
@@ -3,11 +3,10 @@
 curl -LO --proto-redir =https https://web.archive.org/web/abeelabs.com/downloads/PS32.zip
 openssl dgst -sha256 PS32.zip | grep -q 'c9a9fb6d81054468d2884a913275184a318be8a5c830bc901376ae88e720849d' || exit 1
 
-unzip -qo PS32.zip
-
-cp 'PS32/xHarbour/PScript.ch' .
-cp 'PS32/xHarbour/TPSCRIPT.PRG' .
-cp 'PS32/Demos/xbase sources/PSTest.prg' .
+unzip -qoj PS32.zip \
+  'PS32/xHarbour/PScript.ch' \
+  'PS32/xHarbour/TPSCRIPT.PRG' \
+  'PS32/Demos/xbase sources/PSTest.prg'
 
 hbmk2 -sanitize *.ch *.prg *.PRG
 
--- extras/superlib/build.sh
+++ extras/superlib/build.sh
@@ -3,7 +3,7 @@
 curl -O https://harbour.github.io/the-oasis/files/library/supfree.zip
 openssl dgst -sha256 supfree.zip | grep -q '48114a59c8a9ebf51749f74e31dac39e12403180ae2920afb9f93d82794a8fc6' || exit 1
 
-unzip -qo supfree.zip
+unzip -qo supfree.zip SOURCE.ZIP SAMPLES.ZIP
 unzip -qo SOURCE.ZIP
 
 hbmk2 -sanitize *.C *.PRG
