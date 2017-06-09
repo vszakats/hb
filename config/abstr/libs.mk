43462bd23e685c646436a79cb262fdda6bb7e642
Viktor Szakats <vszakats@users.noreply.github.com>
2017-06-09 01:25:43 +0000

    2017-06-09 01:10 UTC Viktor Szakats (vszakats users.noreply.github.com)   + config/abstr/global.mk   + config/abstr/libs.mk   + config/abstr/wasm.mk   * utils/hbmk2/hbmk2.prg   * contrib/hbrun/hbrun.hbp     + add basic support for build Harbour into JS/WebAssembly using       emscripten, using these settings:         HB_PLATFORM=abstr HB_COMPILER=wasm HB_BUILD_3RDEXT=no       Sample hbmk2 compiled into JS, runnable in a browser:         https://transfer.sh/vSGpn/hbmk2js.zip         (download available for 14 days)     ; platform/compiler names not final. [EXPERIMENTAL]
      * utils/hbmk2/hbmk2.prg
        + allow to specify hb_ProgName() value at build-time for platforms
          where the function cannot return a meaningful value (f.e.
          JS/WebAssembly)
        ; TODO: Add support for passing hb_ProgName() value command-line
                parameter, f.e. `--hb:self=hbrun`
    
      * config/global.mk
      * include/hbsetup.h
        * consider abstract/emscripten targets similar to linux-like
    
      * src/rtl/arc4.c
        ! fix to build under emscripten (which is assumed to be a
          linux-like environment)
    
      * contrib/hbdoc/ngjson2hbdoc.hb
        * whitespace

---
+++ config/abstr/libs.mk
@@ -0,0 +1,29 @@
+SYSLIBPATHS :=
+
+ifneq ($(HB_LINKING_RTL),)
+   ifneq ($(HB_HAS_X11),)
+      SYSLIBS += X11
+      ifneq ($(wildcard /usr/X11R6/lib),)
+         SYSLIBPATHS += /usr/X11R6/lib
+      endif
+      ifneq ($(wildcard /opt/X11/lib),)
+         SYSLIBPATHS += /opt/X11/lib
+      endif
+   endif
+   ifneq ($(HB_HAS_PCRE2),)
+      ifeq ($(HB_HAS_PCRE2_LOCAL),)
+         SYSLIBS += pcre2-8
+      endif
+   else
+   ifneq ($(HB_HAS_PCRE),)
+      ifeq ($(HB_HAS_PCRE_LOCAL),)
+         SYSLIBS += pcre
+      endif
+   endif
+   endif
+   ifeq ($(HB_HAS_ZLIB_LOCAL),)
+      SYSLIBS += z
+   endif
+endif
+
+SYSLIBS += m
