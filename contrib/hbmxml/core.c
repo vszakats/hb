53516853bb9c271be827a20dc13c32add3b2fc6b
Viktor Szakats <vszakats@users.noreply.github.com>
2017-08-14 00:28:41 +0000

    2017-08-14 00:28 UTC Viktor Szakats (vszakats users.noreply.github.com) [ci skip]   * contrib/hbmxml/core.c   * tests/setkeys.prg     * copyright header cleanups

--- contrib/hbmxml/core.c
+++ contrib/hbmxml/core.c
@@ -41,6 +41,7 @@
  * If you write modifications of your own for Harbour, it is your choice
  * whether to permit this exception to apply to your modifications.
  * If you do not wish that, delete this exception notice.
+ *
  */
 
 #include "hbapi.h"
