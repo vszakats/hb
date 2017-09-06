vszakats/hb

commit c0c173b8526afb392e64a2dd867315a87163a1fd

2017-09-05 21:16 UTC Viktor Szakats (vszakats users.noreply.github.com)   * utils/hbmk2/hbmk2.prg     % add new functions to the generated .hbx files in full lowercase.       This should help adding manual casing (e.g. camelcase) with much       less effort or it may often be left as-is.

utils/hbmk2/hbmk2.prg
@@ -15804,7 +15804,7 @@ STATIC FUNCTION __hb_extern_get_list_hrb( cInputName )
       aExtern := {}
       hExtern := { => }
       FOR EACH cFunction IN hb_hrbGetFunList( hrb, HB_HRB_FUNC_PUBLIC )
-         cFunction := hb_asciiUpper( cFunction )
+         cFunction := hb_asciiLower( cFunction )
          IF ! cFunction $ hExtern
             AAdd( aExtern, cFunction )
             hExtern[ cFunction ] := NIL
@@ -15867,7 +15867,7 @@ STATIC FUNCTION __hb_extern_get_list( hbmk, cInputName, cBin_LibHBX, cOpt_LibHBX
                aExtern := {}
                hExtern := { => }
                FOR EACH tmp IN aResult
-                  tmp[ 2 ] := hb_asciiUpper( tmp[ 2 ] )
+                  tmp[ 2 ] := hb_asciiLower( tmp[ 2 ] )
                   IF ! tmp[ 2 ] $ hExtern
                      AAdd( aExtern, tmp[ 2 ] )
                      hExtern[ tmp[ 2 ] ] := NIL

commit b38f5e192b93e2d537911f3248ca2c88437f4810

2017-09-06 08:55 UTC Viktor Szakats (vszakats users.noreply.github.com)   * config/postinst.hb     + follow hbmk2 and add new symbols to .hbx in lowercase
  * contrib/hbcurl/easy.c
    * silence a potential fallthrough warning

  * utils/hbmk2/hbmk2.prg
    ! fix a regression after
      c0c173b8526afb392e64a2dd867315a87163a1fd 2017-09-05 21:16 UTC

config/postinst.hb
@@ -817,7 +817,7 @@ STATIC FUNCTION __hb_extern_get_list( cInputName )
                aExtern := {}
                hExtern := { => }
                FOR EACH tmp IN aResult
-                  tmp[ 2 ] := hb_asciiUpper( tmp[ 2 ] )
+                  tmp[ 2 ] := hb_asciiLower( tmp[ 2 ] )
                   IF ! tmp[ 2 ] $ hExtern
                      AAdd( aExtern, tmp[ 2 ] )
                      hExtern[ tmp[ 2 ] ] := NIL
@@ -961,8 +961,8 @@ STATIC FUNCTION __hb_extern_gen( aFuncList, cOutputName )
       NEXT
    ENDIF
    FOR EACH tmp IN aExtern
-      IF ! hb_WildMatch( "HB_GT_*_DEFAULT", tmp, .T. ) .AND. ;
-         ! hb_WildMatch( _HB_SELF_PREFIX + "*" + _HB_SELF_SUFFIX, tmp, .T. ) .AND. ;
+      IF ! hb_WildMatchI( "HB_GT_*_DEFAULT", tmp, .T. ) .AND. ;
+         ! hb_WildMatchI( _HB_SELF_PREFIX + "*" + _HB_SELF_SUFFIX, tmp, .T. ) .AND. ;
          AScan( aExclude, {| flt | hb_WildMatchI( flt, tmp, .T. ) } ) == 0
          cExtern += "DYNAMIC " + hb_HGetDef( hDynamic, tmp, tmp ) + cEOL
       ENDIF

contrib/hbcurl/easy.c
@@ -1225,7 +1225,7 @@ HB_FUNC( CURL_EASY_SETOPT )
                res = curl_easy_setopt( hb_curl->curl, CURLOPT_POST, HB_CURL_OPT_BOOL( 3 ) );
                break;
 #if LIBCURL_VERSION_NUM >= 0x071101
-            case HB_CURLOPT_POSTFIELDS:
+            case HB_CURLOPT_POSTFIELDS: /* fallthrough */
             case HB_CURLOPT_COPYPOSTFIELDS:
                res = curl_easy_setopt( hb_curl->curl, CURLOPT_COPYPOSTFIELDS, hb_parc( 3 ) );
                break;

utils/hbmk2/hbmk2.prg
@@ -16007,8 +16007,8 @@ STATIC FUNCTION __hb_extern_gen( hbmk, aFuncList, cOutputName )
       NEXT
    ENDIF
    FOR EACH tmp IN aExtern
-      IF ! hb_WildMatch( "HB_GT_*_DEFAULT", tmp, .T. ) .AND. ;
-         ! hb_WildMatch( _HB_SELF_PREFIX + "*" + _HB_SELF_SUFFIX, tmp, .T. ) .AND. ;
+      IF ! hb_WildMatchI( "HB_GT_*_DEFAULT", tmp, .T. ) .AND. ;
+         ! hb_WildMatchI( _HB_SELF_PREFIX + "*" + _HB_SELF_SUFFIX, tmp, .T. ) .AND. ;
          AScan( aExclude, {| flt | hb_WildMatchI( flt, tmp, .T. ) } ) == 0
          cExtern += "DYNAMIC " + hb_HGetDef( hDynamic, tmp, tmp ) + cEOL
       ENDIF