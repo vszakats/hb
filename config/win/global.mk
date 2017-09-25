d4fd0c48d6dd9a78cb1af5c1eab414b0978c179c
Viktor Szakats <vszakats@users.noreply.github.com>
2017-09-25 07:44:05 +0000

    config/win/global.mk: comment update [ci skip]

--- config/win/global.mk
+++ config/win/global.mk
@@ -11,11 +11,12 @@ HB_GT_LIBS += gtwvt gtgui gtwin
 HB_SIGN_TIMEURL := http://timestamp.digicert.com
 
 # kernel32: needed by some compilers (pocc/watcom)
-# user32: *Clipboard*(), GetKeyState(), GetKeyboardState(), SetKeyboardState(), gtwvt stuff
+# user32: *Clipboard*(), GetKeyState(), GetKeyboardState(), SetKeyboardState(), gtwvt
 # ws2_32/wsock32: hbsocket
 # ws2_32: WSAIoctl()
 # iphlpapi: hbsocket->GetAdaptersInfo()
 # advapi32: GetUserName()
 # gdi32: gtwvt
+# winmm: timeGetTime()
 
 SYSLIBS += kernel32 user32 ws2_32 iphlpapi advapi32 gdi32 winmm
